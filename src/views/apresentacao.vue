a4f140bc70a23ea03fb3aaac1f6be8620ec59ae4
wanderson <wandersonsilva.zetta@ufla.br>
2021-08-02 16:39:20 -0300

    melhorando apresentacao

--- src/views/apresentacao.vue
+++ src/views/apresentacao.vue
@@ -7,15 +7,14 @@
         <br>
         <b>Olá,</b>
         <br>
-        Eu me chamo Wanderson Calixto da Silva, sou formado em Ciência da Computação e trabalho há três anos como programador.<br>
-        Esta aplicação foi criada a partir do Desafio Prático do processo seletivo da Agência Zetta.<br>
+        Eu me chamo Wanderson Calixto da Silva, sou formado em Ciência da Computação e trabalho há quase três anos com programação.<br>
+        Esta aplicação foi criada a partir do Desafio Prático do processo seletivo da Pulses.<br>
         Entre as diversas tecnologias utilizadas no desenvolvimento, pode-se listar:
        <ul>
             <li><b>Backend</b><ul>
-                    <li>Linguagem: Java</li>
-                    <li>Framework Java: Spring Boot</li>
-                    <li>Banco de Dados: PostgreSQL</li>
-                    <li>Framework para persistencia dos dados: Hibernate</li>
+                    <li>Linguagem: PHP</li>
+                    <li>Framework PHP: Laravel</li>
+                    <li>Banco de Dados: MYSQL</li>             
                 </ul>
             </li>
         </ul>
@@ -27,18 +26,15 @@
             </li>
         </ul>
          <ul>
-            <li><b>Hospedagem</b><ul>
-                    <li>Backend: <a href="https://apirest-zetta.herokuapp.com/swagger-ui.html#/" target="_blank">Heroku</a></li>
+            <li><b>Hospedagem: </b><ul>
                     <li>Frontend: <a href="http://wandersonzetta.online/#/" target="_blank">Umbler</a></li>                  
                 </ul>
             </li>
         </ul>
         <ul>
             <li><b>Repostório</b><ul>
-                    <li><a href="https://github.com/wandcalixto/zetta-backend" target="_blank">Backend</a></li>
-                    <li><a href="https://github.com/wandcalixto/zetta-frontend" target="_blank">Frontend</a></li> 
-                    <li><a href="https://drive.google.com/file/d/1J9qae63PRTfX7swFnwie_FwOhyQcpzpD/view?usp=sharing" target="_blank">Instruções</a></li>      
-                               
+                    <li><a href="https://github.com/wandcalixto/desfio-pulses" target="_blank">Backend</a></li>
+                    <li><a href="https://github.com/wandcalixto/desafio-pulses-frontend" target="_blank">Frontend</a></li>                                
                 </ul>
             </li>
         </ul>
